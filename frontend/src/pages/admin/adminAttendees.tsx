73d3f6775aedce76d884f8013dc041536afc3327
Mahadi Hasan <mahadi.bapari193@gmail.com>
2025-12-04 16:17:15 +0600

    Updated attendees pagination

--- frontend/src/pages/admin/adminAttendees.tsx
+++ frontend/src/pages/admin/adminAttendees.tsx
@@ -43,7 +43,7 @@ export const AdminAttendees: React.FC<AdminAttendeesProps> = ({
   
   // Pagination state
   const [currentPage, setCurrentPage] = useState(1);
-  const attendeesPerPage = 5;
+  const attendeesPerPage = 30;
 
   // State for Add Attendee (select user) modal
   interface SimpleUser {
